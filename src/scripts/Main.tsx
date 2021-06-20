a87c1340c5d2a03f891519ac9d5226661dfb9e06
kdm <kdm1jkmm@gmail.com>
2021-06-20 19:53:34 +0900

    Remove bulma

--- src/scripts/Main.tsx
+++ src/scripts/Main.tsx
@@ -1,16 +1,7 @@
 import React, { Component } from "react";
-import * as Bulma from "react-bulma-components";
 
 export default class Main extends Component {
   render() {
-    return (
-      <Bulma.Block>
-        <Bulma.Columns>
-          <Bulma.Columns.Column is="1">
-            <Bulma.Notification color="primary">ASD</Bulma.Notification>
-          </Bulma.Columns.Column>
-        </Bulma.Columns>
-      </Bulma.Block>
-    );
+    return <h1>Hello</h1>;
   }
 }
