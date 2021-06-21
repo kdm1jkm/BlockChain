7577cab4eeba4aa7aea484ceb32390a6024db106
kdm <kdm1jkmm@gmail.com>
2021-06-21 17:12:05 +0900

    Add dependency of styled-components

--- src/scripts/Main.tsx
+++ src/scripts/Main.tsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import Styled from "styled-components";
+
+const Header = Styled.h1`
+  text-align: center;
+`;
 
 export default class Main extends Component {
   render() {
-    return <h1>Hello</h1>;
+    return <Header>BlockChain Simulator</Header>;
   }
 }
